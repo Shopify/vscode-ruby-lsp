c0824c6a37f6c667ed176e9170cedbdb36b73e96
Vinicius Stock <vinicius.stock@shopify.com>
2022-03-17 15:13:06 -0400

    Activate shadowenv before trying to start LSP

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,14 @@ import Client from "./client";
 let client: Client;
 
 export async function activate(context: vscode.ExtensionContext) {
+  activateRuby();
+
   client = new Client(context);
   await client.start();
+}
+
+function activateRuby() {
+  vscode.extensions.getExtension("shopify.vscode-shadowenv")?.activate();
 }
 
 export async function deactivate() {
