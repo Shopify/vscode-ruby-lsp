6bbd47ccaae37abaf84146b7eb6aaf8489d10143
Vinicius Stock <vinicius.stock@shopify.com>
2023-12-01 17:07:17 -0500

    Implement internal Ruby activation mechanism

--- src/test/suite/ruby.test.ts
+++ src/test/suite/ruby.test.ts
@@ -3,41 +3,174 @@ import * as fs from "fs";
 import * as path from "path";
 import * as os from "os";
 
+import { beforeEach, afterEach } from "mocha";
 import * as vscode from "vscode";
 
-import { Ruby, VersionManager } from "../../ruby";
+import { Ruby } from "../../ruby";
+
+const PATH_SEPARATOR = os.platform() === "win32" ? ";" : ":";
 
 suite("Ruby environment activation", () => {
   let ruby: Ruby;
+  let tmpPath: string;
+  const context = {
+    extensionMode: vscode.ExtensionMode.Test,
+    subscriptions: [],
+    workspaceState: {
+      get: (_name: string) => undefined,
+      update: (_name: string, _value: any) => Promise.resolve(),
+    },
+  } as unknown as vscode.ExtensionContext;
 
-  test("Activate fetches Ruby information when outside of Ruby LSP", async () => {
-    if (os.platform() !== "win32") {
-      // eslint-disable-next-line no-process-env
-      process.env.SHELL = "/bin/bash";
+  beforeEach(() => {
+    tmpPath = fs.mkdtempSync(path.join(os.tmpdir(), "ruby-lsp-test-"));
+    ruby = new Ruby(
+      {
+        uri: { fsPath: tmpPath },
+      } as vscode.WorkspaceFolder,
+      context,
+    );
+  });
+
+  afterEach(() => {
+    fs.rmSync(tmpPath, { recursive: true, force: true });
+  });
+
+  test("fetches Ruby environment for .ruby-version", async () => {
+    fs.writeFileSync(path.join(tmpPath, ".ruby-version"), "3.2.2");
+    const rubyEnv = await ruby.activate();
+
+    assert.strictEqual(rubyEnv.RUBY_VERSION, "3.2.2");
+    assert.match(rubyEnv.GEM_HOME, /.gem\/ruby\/3.2.0/);
+    assert.match(
+      rubyEnv.GEM_ROOT,
+      /3.2.2\/([A-Za-z0-9]+\/)?lib\/ruby\/gems\/3.2.0/,
+    );
+    assert.strictEqual(
+      rubyEnv.GEM_PATH,
+      `${rubyEnv.GEM_HOME}${PATH_SEPARATOR}${rubyEnv.GEM_ROOT}`,
+    );
+  });
+
+  test("fetches Ruby environment for global rbenv version", async () => {
+    const dir = path.join(os.homedir(), ".rbenv");
+    const shouldRemoveDir = !fs.existsSync(dir);
+
+    if (shouldRemoveDir) {
+      fs.mkdirSync(dir);
     }
 
-    const tmpPath = fs.mkdtempSync(path.join(os.tmpdir(), "ruby-lsp-test-"));
+    const versionPath = path.join(dir, "version");
+    let originalVersion;
+    if (fs.existsSync(versionPath)) {
+      originalVersion = fs.readFileSync(versionPath, "utf8");
+    }
+
+    fs.writeFileSync(versionPath, "3.2.2");
+    const rubyEnv = await ruby.activate();
+
+    assert.strictEqual(rubyEnv.RUBY_VERSION, "3.2.2");
+    assert.match(rubyEnv.GEM_HOME, /.gem\/ruby\/3.2.0/);
+    assert.match(
+      rubyEnv.GEM_ROOT,
+      /3.2.2\/([A-Za-z0-9]+\/)?lib\/ruby\/gems\/3.2.0/,
+    );
+    assert.strictEqual(
+      rubyEnv.GEM_PATH,
+      `${rubyEnv.GEM_HOME}${PATH_SEPARATOR}${rubyEnv.GEM_ROOT}`,
+    );
+
+    if (shouldRemoveDir) {
+      fs.rmSync(dir, { recursive: true, force: true });
+    }
+
+    if (originalVersion) {
+      fs.writeFileSync(versionPath, originalVersion);
+    }
+  });
+
+  test("fetches Ruby environment for .ruby-version using engine", async () => {
+    fs.writeFileSync(path.join(tmpPath, ".ruby-version"), "ruby-3.2.2");
+    const rubyEnv = await ruby.activate();
+
+    assert.strictEqual(rubyEnv.RUBY_VERSION, "3.2.2");
+    assert.match(rubyEnv.GEM_HOME, /.gem\/ruby\/3.2.0/);
+    assert.match(
+      rubyEnv.GEM_ROOT,
+      /3.2.2\/([A-Za-z0-9]+\/)?lib\/ruby\/gems\/3.2.0/,
+    );
+    assert.strictEqual(
+      rubyEnv.GEM_PATH,
+      `${rubyEnv.GEM_HOME}${PATH_SEPARATOR}${rubyEnv.GEM_ROOT}`,
+    );
+  });
+
+  test("fetches Ruby environment for .ruby-version with .ruby-gemset", async () => {
     fs.writeFileSync(path.join(tmpPath, ".ruby-version"), "3.2.2");
+    fs.writeFileSync(path.join(tmpPath, ".ruby-gemset"), "hello");
+    const rubyEnv = await ruby.activate();
 
-    const context = {
-      extensionMode: vscode.ExtensionMode.Test,
-    } as vscode.ExtensionContext;
-
-    ruby = new Ruby(context, {
-      uri: { fsPath: tmpPath },
-    } as vscode.WorkspaceFolder);
-    await ruby.activateRuby(
-      // eslint-disable-next-line no-process-env
-      process.env.CI ? VersionManager.None : VersionManager.Chruby,
+    assert.strictEqual(rubyEnv.RUBY_VERSION, "3.2.2");
+    assert.match(rubyEnv.GEM_HOME, /.gem\/ruby\/3.2.0@hello/);
+    assert.match(
+      rubyEnv.GEM_ROOT,
+      /3.2.2\/([A-Za-z0-9]+\/)?lib\/ruby\/gems\/3.2.0/,
     );
-
-    assert.ok(ruby.rubyVersion, "Expected Ruby version to be set");
-    assert.notStrictEqual(
-      ruby.supportsYjit,
-      undefined,
-      "Expected YJIT support to be set to true or false",
+    assert.strictEqual(
+      rubyEnv.GEM_PATH,
+      `${rubyEnv.GEM_HOME}${PATH_SEPARATOR}${rubyEnv.GEM_ROOT}`,
     );
+  });
 
-    fs.rmSync(tmpPath, { recursive: true, force: true });
+  test("fetches Ruby environment for dev.yml", async () => {
+    fs.writeFileSync(path.join(tmpPath, "dev.yml"), "- ruby: '3.2.2'");
+    const rubyEnv = await ruby.activate();
+
+    assert.strictEqual(rubyEnv.RUBY_VERSION, "3.2.2");
+    assert.match(rubyEnv.GEM_HOME, /.gem\/ruby\/3.2.0/);
+    assert.match(
+      rubyEnv.GEM_ROOT,
+      /3.2.2\/([A-Za-z0-9]+\/)?lib\/ruby\/gems\/3.2.0/,
+    );
+    assert.strictEqual(
+      rubyEnv.GEM_PATH,
+      `${rubyEnv.GEM_HOME}${PATH_SEPARATOR}${rubyEnv.GEM_ROOT}`,
+    );
+  });
+
+  test("fetches Ruby environment for .tool-versions", async () => {
+    fs.writeFileSync(path.join(tmpPath, ".tool-versions"), "ruby 3.2.2");
+    const rubyEnv = await ruby.activate();
+
+    assert.strictEqual(rubyEnv.RUBY_VERSION, "3.2.2");
+    assert.match(rubyEnv.GEM_HOME, /.gem\/ruby\/3.2.0/);
+    assert.match(
+      rubyEnv.GEM_ROOT,
+      /3.2.2\/([A-Za-z0-9]+\/)?lib\/ruby\/gems\/3.2.0/,
+    );
+    assert.strictEqual(
+      rubyEnv.GEM_PATH,
+      `${rubyEnv.GEM_HOME}${PATH_SEPARATOR}${rubyEnv.GEM_ROOT}`,
+    );
+  });
+
+  test("fetches Ruby environment for .rtx.toml", async () => {
+    fs.writeFileSync(
+      path.join(tmpPath, ".rtx.toml"),
+      `[tools]
+       ruby = '3.2.2'`,
+    );
+    const rubyEnv = await ruby.activate();
+
+    assert.strictEqual(rubyEnv.RUBY_VERSION, "3.2.2");
+    assert.match(rubyEnv.GEM_HOME, /.gem\/ruby\/3.2.0/);
+    assert.match(
+      rubyEnv.GEM_ROOT,
+      /3.2.2\/([A-Za-z0-9]+\/)?lib\/ruby\/gems\/3.2.0/,
+    );
+    assert.strictEqual(
+      rubyEnv.GEM_PATH,
+      `${rubyEnv.GEM_HOME}${PATH_SEPARATOR}${rubyEnv.GEM_ROOT}`,
+    );
   });
 });
